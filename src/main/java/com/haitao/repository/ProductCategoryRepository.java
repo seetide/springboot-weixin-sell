e22b36af102857e5b9aa6cb67faa45aa70ebd33d
Haitao Gu <haitaogu@MacBook-Pro.home>
2020-09-26 19:32:29 -0400

    test the repository

---
+++ src/main/java/com/haitao/repository/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.haitao.repository;
+
+import com.haitao.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+//@Component
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+}
---
+++ src/test/java/com/haitao/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.haitao.repository;
+
+import com.haitao.dataobject.ProductCategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest() {
+        Optional<ProductCategory> category = repository.findById(1);
+        System.out.println(category.toString());
+    }
+}
