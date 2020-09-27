a26f7b1e62c27bb4af184bd6dc2d3658b359d77f
Haitao Gu <haitaogu@MacBook-Pro.home>
2020-09-27 11:15:28 -0400

    small change add controller

---
+++ src/main/java/com/haitao/controller/BuyerProductController.java
@@ -0,0 +1,10 @@
+package com.haitao.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/buyer/product")
+public class BuyerProductController {
+}
